5cc4b516ec52e774ea231157632b15de93e5d692
raul-3520yahoo <95866693+raul-3520yahoo@users.noreply.github.com>
2021-12-30 10:25:17 +0530

    Update k8monit.sh

--- k8monit.sh
+++ k8monit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #sleep 180
-podstat=`kubectl get pod -A`
-stat=`kubectl get pod -A |grep -v NAME|grep -v Running|wc -l`
-echo $stat
-if [ $stat -gt 0 ]
+podstat=$(kubectl get pod -A)
+stat=$(kubectl get pod -A |grep -v NAME|grep -v Running|wc -l)
+echo "$stat"
+if [ "$stat" -gt 0 ]
 then
 	echo "Check pod status,there is some potential error"
 	kubectl get pod -A |grep -v NAME|grep -v Running
 	exit 1
 else
-	echo "Pod is up and running \n $podstat"
+	printf "Pod is up and running \n $podstat"
 
 fi
 
